023a3e1fb8987a685a3493f092f613a3e489b659
hector.gv00 <hector.gv00@gmail.com>
2022-10-10 19:26:27 +0200

    Las x y las o se pintan en el tablero, se añade su valor a la array cuadricula y se borra el display y la cuadricula al darle al boton start

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ class Jugador {
     }
 }
 
-let cuadricula =["0","0","0","0","0","0","0","0","0"]
+let cuadricula =["","","","","","","","",""]
 
 const botonStart = document.getElementById("boton-start");
 const textoMuestra = document.getElementById("texto-muestra");
@@ -22,31 +22,47 @@ const winCondition = [
 ];
 
 
-// Lo que yo he utilizado para el local storage:  listaJugadores = JSON.parse(localStorage.getItem('NOMBRE_QUE_LE_QUERAIS_DAR')); /  localStorage.setItem('NOMBRE_QUE_LE_QUERAIS_DAR', JSON.stringify(listaJugadores));
-
 
 
 // -------------------------------------------------------------------------------------
 
     // Cambiar las casillas
 
-let casilla11 =  document.getElementById("cuadro11")
-let casilla12 =  document.getElementById("cuadro12")
-let casilla13 =  document.getElementById("cuadro13")
-let casilla21 =  document.getElementById("cuadro21")
-let casilla22 =  document.getElementById("cuadro22")
-let casilla23 =  document.getElementById("cuadro23")
-let casilla31 =  document.getElementById("cuadro31")
-let casilla32 =  document.getElementById("cuadro32")
-let casilla33 =  document.getElementById("cuadro33")
+let casillas = Array.from(document.getElementsByClassName("casilla"));
+let interruptor = true;
 
+
+
+
+const textCasillas = ()=>{
+ casillas.map((casilla, index) => {
+    casilla.addEventListener("click", () => {
+        if (casilla.innerHTML == "") {
+            casilla.innerHTML = (interruptor) ? `<h3 class="color-naranja-electrico efecto-glitch">X</h3>
+            </div>`: `<h3 class="color-azul-electrico efecto-glitch">O</h3>
+            </div>`;
+            if(interruptor){
+                textoMuestra.innerHTML= `Turno de ${sessionStorage.getItem("jugador2")}`;
+                cuadricula[index] = "X";
+        }else{
+            textoMuestra.innerHTML= `Turno de ${sessionStorage.getItem("jugador1")}`;
+            cuadricula[index] = "O";
+        }   
+
+            interruptor = !interruptor;
+
+            //Comprobamos en otra funcion si hay un ganador.......
+
+        };
+    });
+});
+}
 
 
 // Creación de objetos player
 
 let player1 = new Jugador(sessionStorage.getItem("jugador1"), "Placeholder")
 
-console.log(player1);
 
 
 
@@ -55,21 +71,15 @@ let contadorTurnos =0;
 
 
 const BucleJuego =()=>{
-   for(let i = 0; i<10; i++){
-        contadorTurnos++
-        if(contadorTurnos%2==0){
-            textoMuestra.innerHTML= `Turno de ${sessionStorage.getItem("jugador1")}`
-        }else{
-            textoMuestra.innerHTML= `Turno de ${sessionStorage.getItem("jugador2")}`
-        }
-        console.log("hola");
-        casilla11.addEventListener("click", Cambiocasilla=()=>{ if(contadorTurnos%2==0){
-            document.getElementById("texto-11").innerHTML = "X"
-        }else{
-            document.getElementById("texto-11").innerHTML = "O"
-        }
-    });
+
+    for (let i = 0; i < cuadricula.length; i++) {
+        cuadricula[i] = "";
     }
+    casillas.map((casilla, index)=>{casilla.innerHTML = ""});
+
+    textoMuestra.innerHTML= `Turno de ${sessionStorage.getItem("jugador1")}`
+    textCasillas()
+    
 }
 
 
